4463d17ff2f15405dd800095dcb1bf27d4b511a9
$@ID <73528996+00009713@users.noreply.github.com>
2023-01-18 16:08:29 +0500

    .

---
+++ src/XMLHandler.java
@@ -0,0 +1,25 @@
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+import java.sql.SQLException;
+
+public class XMLHandler extends DefaultHandler
+{
+    int limit = 30_000_000;
+    int number = 0;
+
+    @Override
+    public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+        if (qName.equals("voter") && number < limit){
+            String name = attributes.getValue("name");
+            String birthDate = attributes.getValue("birthDay");
+            try {
+                DBConnection.countVoter(name, birthDate);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            number++;
+        }
+    }
+}
